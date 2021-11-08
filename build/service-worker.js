8db0a7351116432c83a1b99e4705c9bfa7b343b3
hereusmizo <hereusmizo@gmail.com>
2021-11-08 14:42:33 +0530

    11/08/2021 14:42:33

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bb62960d0c28983bf7a880540f5d8a91.js"
+  "/precache-manifest.86979b155403ffeefcd61d17bc5930e7.js"
 );
 
 self.addEventListener('message', (event) => {
